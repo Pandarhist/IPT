c30f5b46892bd7b7bb1dfd53402aa9033f65a2b3
Pandarhist <kazimov.nikita1@mail.ru>
2022-11-14 02:26:25 +0300

    Added containers for images, updated page styles.
    * Added shadow effect on hovering image;
    * Added flex container for images;
    * Added sections in documents.

--- Lab5/js/task2.js
+++ Lab5/js/task2.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const scale = 100;
-
 const firstImg = document.getElementById('first-img');
 const secondImg = document.getElementById('second-img');
 const thirdImg = document.getElementById('third-img');
@@ -10,12 +8,12 @@ firstImg.src = "../resources/picture1.png";
 secondImg.src = "../resources/picture2.png";
 thirdImg.src = "../resources/picture3.png";
 
-firstImg.addEventListener('mouseenter', () => { scaleImg(firstImg, scale) });
-firstImg.addEventListener('mouseleave', () => { scaleImg(firstImg, -scale) });
-secondImg.addEventListener('mouseenter', () => { scaleImg(secondImg, scale) });
-secondImg.addEventListener('mouseleave', () => { scaleImg(secondImg, -scale) });
-thirdImg.addEventListener('mouseenter', () => { scaleImg(thirdImg, scale) });
-thirdImg.addEventListener('mouseleave', () => { scaleImg(thirdImg, -scale) });
+firstImg.addEventListener('mouseenter', () => scaleImg(firstImg));
+firstImg.addEventListener('mouseleave', () => scaleImg(firstImg));
+secondImg.addEventListener('mouseenter', () => scaleImg(secondImg));
+secondImg.addEventListener('mouseleave', () => scaleImg(secondImg));
+thirdImg.addEventListener('mouseenter', () => scaleImg(thirdImg));
+thirdImg.addEventListener('mouseleave', () => scaleImg(thirdImg));
 
 function scaleImg(img, scale){
     img.width += scale;
