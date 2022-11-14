7fea707187731fb16755ad8b4f8c4fd2ad1ef5ab
Pandarhist <kazimov.nikita1@mail.ru>
2022-11-14 03:29:52 +0300

    Added script for highlighting menu, updated styles.

--- Lab5/js/task2.js
+++ Lab5/js/task2.js
@@ -4,18 +4,17 @@ const firstImg = document.getElementById('first-img');
 const secondImg = document.getElementById('second-img');
 const thirdImg = document.getElementById('third-img');
 
-firstImg.src = "../resources/picture1.png";
-secondImg.src = "../resources/picture2.png";
-thirdImg.src = "../resources/picture3.png";
-
 firstImg.addEventListener('mouseenter', () => scaleImg(firstImg));
-firstImg.addEventListener('mouseleave', () => scaleImg(firstImg));
+firstImg.addEventListener('mouseleave', () => noScale(firstImg));
 secondImg.addEventListener('mouseenter', () => scaleImg(secondImg));
-secondImg.addEventListener('mouseleave', () => scaleImg(secondImg));
+secondImg.addEventListener('mouseleave', () => noScale(secondImg));
 thirdImg.addEventListener('mouseenter', () => scaleImg(thirdImg));
-thirdImg.addEventListener('mouseleave', () => scaleImg(thirdImg));
+thirdImg.addEventListener('mouseleave', () => noScale(thirdImg));
 
-function scaleImg(img, scale){
-    img.width += scale;
-    img.height += scale;
+function scaleImg(img){
+    img.classList.add('image-transformed');
+}
+
+function noScale(img) {
+    img.classList.remove('image-transformed');
 }
